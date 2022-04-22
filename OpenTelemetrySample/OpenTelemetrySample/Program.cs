3eb540d45303c890ccb4487575afa184ff9cac2d
Michael Murphy <michael.murphy@emailage.com>
2022-04-22 12:01:25 +0100

    Enable swagger

--- OpenTelemetrySample/OpenTelemetrySample/Program.cs
+++ OpenTelemetrySample/OpenTelemetrySample/Program.cs
@@ -80,13 +80,8 @@ builder.Logging.AddOpenTelemetry(options =>
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
